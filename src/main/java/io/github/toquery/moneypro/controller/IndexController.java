6d16030daebf367ef5d9fb2274ac532025969681
ToQuery <toquery@qq.com>
2019-01-31 11:12:28 +0800

    添加前端项目

--- src/main/java/io/github/toquery/moneypro/controller/IndexController.java
+++ src/main/java/io/github/toquery/moneypro/controller/IndexController.java
@@ -2,7 +2,9 @@ package io.github.toquery.moneypro.controller;
 
 import io.github.toquery.moneypro.MoneyProData;
 import io.github.toquery.moneypro.entity.MoneyPro;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -12,15 +14,21 @@ import java.util.List;
  * @author toquery
  * @version 1
  */
-@RestController
+@Controller
 public class IndexController {
 
     @Resource
     private MoneyProData moneyProData;
 
 
+    @ResponseBody
     @RequestMapping("/")
     public List<MoneyPro> index() {
         return moneyProData.getData();
+    }
+
+    @RequestMapping("/index")
+    public String in(){
+        return "dist/index.html";
     }
 }
